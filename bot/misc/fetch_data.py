4068ad4bdb23ef3a1079faecf6985a4741c039ff
Matvey <matveykawork@gmail.com>
2023-08-02 22:22:21 +0300

    fixed some bugs

--- bot/misc/fetch_data.py
+++ bot/misc/fetch_data.py
@@ -1,9 +1,9 @@
 from aiohttp import ClientSession
 
 
-async def get_data_from_server(url: str, c_id: int=2):
+async def get_data_from_server(url: str, c_id: int=None):
     async with ClientSession() as session:
-        if id:
+        if c_id:
             url += f'{c_id}/'
             # print(url)
         async with session.get(url) as res:
